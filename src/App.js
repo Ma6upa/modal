d420dc9126335665fbc928511ae964bd2570632a
Ma6upa <vaud55@gmail.com>
2023-05-01 16:20:07 +0400

    add button, modal and delete unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  Modal,
+  Typography,
+} from '@mui/material';
 
 function App() {
+  const theme = createTheme();
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleClose = () => {
+    setOpenModal(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Button
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={() => {
+                setOpenModal(true)
+              }}
+            >
+              Выполнить действие
+            </Button>
+          </Box>
+        </Container>
+      </ThemeProvider>
+
+      <Modal
+        open={openModal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Container component="main" maxWidth="sm">
+          <Box styles={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                backgroundColor: '#ffffff',
+                marginTop: '30%',
+                borderRadius: 5,
+              }}
+            >
+              <Typography variant="h6">
+                Согласие с правилами
+              </Typography>
+              <Box sx={{
+                width: '90%'
+              }}>
+                <Button
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, float: 'right' }}
+                  onClick={() => {
+                    setOpenModal(false)
+                  }}
+                >
+                  Сохранить
+                </Button>
+                <Button
+                  variant="outlined"
+                  sx={{ mt: 3, mb: 2, float: 'right', mr: 2 }}
+                  onClick={() => {
+                    setOpenModal(false)
+                  }}
+                >
+                  Отменить
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Container>
+      </Modal>
+    </>
   );
 }
 
